96d5fafa59cedfed9afcb976d580f16c9b98996e
spuxx1701 <accounts@spuxx.dev>
2025-03-13 22:58:19 +0100

    docs: Document `AppBar` component

--- packages/solid/src/components/layout/app-bar/app-bar.component.tsx
+++ packages/solid/src/components/layout/app-bar/app-bar.component.tsx
@@ -2,10 +2,35 @@ import { AppBarProps, attributes, classNames } from '@src/main';
 import { Component, ParentProps } from 'solid-js';
 import { Dynamic } from 'solid-js/web';
 
+/**
+ * A section of the app bar.
+ * @param props - {@link ParentProps}
+ * @returns The section component.
+ */
 const Section: Component<ParentProps> = (props) => (
   <div {...classNames('spx-app-bar-section')}>{props.children}</div>
 );
 
+/**
+ * A flexibile app bar component that can be used to display content and actions related
+ * to the current screen.
+ * @param props - {@link AppBarProps}
+ * @returns The app bar component.
+ * @example
+ * ```tsx
+ * <AppBar>
+ *   <AppBar.Section>
+ *     Left content
+ *   </AppBar.Section>
+ *   <AppBar.Section>
+ *     Center content
+ *   </AppBar.Section>
+ *   <AppBar.Section>
+ *     Right content
+ *   </AppBar.Section>
+ * </AppBar>
+ * ```
+ */
 const AppBar: Component<AppBarProps> & {
   Section: Component<ParentProps>;
 } = ((props) => {
