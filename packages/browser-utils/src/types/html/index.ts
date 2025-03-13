fcd36feaac438aa67de256062dc3ae917fbe7950
@LeopoldHock <leopold.hock@deutschebahn.com>
2025-03-13 11:48:12 +0100

    chore: Convert InputType to JavaScript object

--- packages/browser-utils/src/types/html/index.ts
+++ packages/browser-utils/src/types/html/index.ts
@@ -2,26 +2,32 @@
  * Valid input types according to the HTML specification.
  * @see {@link https://developer.mozilla.org/en-US/docs/Web/HTML/Element/input#input_types}
  */
-export type InputType =
-  | 'button'
-  | 'checkbox'
-  | 'color'
-  | 'date'
-  | 'datetime-local'
-  | 'email'
-  | 'file'
-  | 'hidden'
-  | 'image'
-  | 'month'
-  | 'number'
-  | 'password'
-  | 'radio'
-  | 'range'
-  | 'reset'
-  | 'search'
-  | 'submit'
-  | 'tel'
-  | 'text'
-  | 'time'
-  | 'url'
-  | 'week';
+export const InputType = {
+  button: 'button',
+  checkbox: 'checkbox',
+  color: 'color',
+  date: 'date',
+  datetimeLocal: 'datetime-local',
+  email: 'email',
+  file: 'file',
+  hidden: 'hidden',
+  image: 'image',
+  month: 'month',
+  number: 'number',
+  password: 'password',
+  radio: 'radio',
+  range: 'range',
+  reset: 'reset',
+  search: 'search',
+  submit: 'submit',
+  tel: 'tel',
+  text: 'text',
+  time: 'time',
+  url: 'url',
+  week: 'week',
+} as const;
+/**
+ * Valid input types according to the HTML specification.
+ * @see {@link https://developer.mozilla.org/en-US/docs/Web/HTML/Element/input#input_types}
+ */
+export type InputType = (typeof InputType)[keyof typeof InputType];
