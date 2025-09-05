eef92f0d959ebe4835c2691458160de3cedb45e5
@LeopoldHock <leopold.hock@deutschebahn.com>
2025-09-05 11:53:27 +0200

    style(apps): Improve format

--- apps/solid/src/routes/components/control/select.route.tsx
+++ apps/solid/src/routes/components/control/select.route.tsx
@@ -16,127 +16,125 @@ export const SelectRoute: Component = () => {
   };
 
   return (
-    <>
-      <Container tag="article">
-        <Heading level={1}>Input</Heading>
+    <Container tag="article">
+      <Heading level={1}>Input</Heading>
+      <Divider color="gradient" />
+      <Container variant="contained" color="background">
+        <Heading level={2}>Variants</Heading>
         <Divider color="gradient" />
-        <Container variant="contained" color="background">
-          <Heading level={2}>Variants</Heading>
-          <Divider color="gradient" />
-          <Select
-            label="contained (default)"
-            options={options}
-            size="small"
-            class="m-1"
-            onChange={handleChange}
-          />
-          <Select
-            label="outlined"
-            options={options}
-            variant="outlined"
-            size="small"
-            class="m-1"
-            onChange={handleChange}
-          />
-        </Container>
-        <Container variant="contained" color="background">
-          <Heading level={2}>Initial Value</Heading>
-          <Divider color="gradient" />
-          <Select
-            label="With option"
-            default={options[1]}
-            options={options}
-            class="m-1"
-            size="small"
-            onChange={handleChange}
-          />
-          <Select
-            label="With index"
-            default={1}
-            options={options}
-            class="m-1"
-            size="small"
-            onChange={handleChange}
-          />
-        </Container>
-        <Container variant="contained" color="background">
-          <Heading level={2}>Sizes</Heading>
-          <Divider color="gradient" />
-          <For each={variants}>
-            {(variant) => (
-              <>
-                <Select
-                  label="auto (default)"
-                  options={options}
-                  class="m-1"
-                  variant={variant}
-                  onChange={handleChange}
-                />
-                <Select
-                  label="small"
-                  options={options}
-                  class="m-1"
-                  size="small"
-                  variant={variant}
-                  onChange={handleChange}
-                />
-                <Select
-                  label="medium"
-                  options={options}
-                  class="m-1"
-                  size="medium"
-                  variant={variant}
-                  onChange={handleChange}
-                />
-                <Select
-                  label="large"
-                  options={options}
-                  class="m-1"
-                  size="large"
-                  variant={variant}
-                  onChange={handleChange}
-                />
-                <Select
-                  label="full"
-                  options={options}
-                  class="m-1"
-                  size="full"
-                  variant={variant}
-                  onChange={handleChange}
-                />
-              </>
-            )}
-          </For>
-        </Container>
-        <Container variant="contained" color="background">
-          <Heading level={2}>With Icon</Heading>
-          <Divider color="gradient" />
-          <For each={variants}>
-            {(variant) => (
-              <>
-                <Select
-                  label="Fruit"
-                  class="m-1"
-                  icon="mdi:fruit-cherries"
-                  size="small"
-                  variant={variant}
-                  options={options}
-                  onChange={handleChange}
-                />
-                <Select
-                  label="Fruit"
-                  class="m-1"
-                  icon="mdi:fruit-cherries"
-                  size="small"
-                  variant={variant}
-                  options={options}
-                  onChange={handleChange}
-                />
-              </>
-            )}
-          </For>
-        </Container>
+        <Select
+          label="contained (default)"
+          options={options}
+          size="small"
+          class="m-1"
+          onChange={handleChange}
+        />
+        <Select
+          label="outlined"
+          options={options}
+          variant="outlined"
+          size="small"
+          class="m-1"
+          onChange={handleChange}
+        />
       </Container>
-    </>
+      <Container variant="contained" color="background">
+        <Heading level={2}>Initial Value</Heading>
+        <Divider color="gradient" />
+        <Select
+          label="With option"
+          default={options[1]}
+          options={options}
+          class="m-1"
+          size="small"
+          onChange={handleChange}
+        />
+        <Select
+          label="With index"
+          default={1}
+          options={options}
+          class="m-1"
+          size="small"
+          onChange={handleChange}
+        />
+      </Container>
+      <Container variant="contained" color="background">
+        <Heading level={2}>Sizes</Heading>
+        <Divider color="gradient" />
+        <For each={variants}>
+          {(variant) => (
+            <>
+              <Select
+                label="auto (default)"
+                options={options}
+                class="m-1"
+                variant={variant}
+                onChange={handleChange}
+              />
+              <Select
+                label="small"
+                options={options}
+                class="m-1"
+                size="small"
+                variant={variant}
+                onChange={handleChange}
+              />
+              <Select
+                label="medium"
+                options={options}
+                class="m-1"
+                size="medium"
+                variant={variant}
+                onChange={handleChange}
+              />
+              <Select
+                label="large"
+                options={options}
+                class="m-1"
+                size="large"
+                variant={variant}
+                onChange={handleChange}
+              />
+              <Select
+                label="full"
+                options={options}
+                class="m-1"
+                size="full"
+                variant={variant}
+                onChange={handleChange}
+              />
+            </>
+          )}
+        </For>
+      </Container>
+      <Container variant="contained" color="background">
+        <Heading level={2}>With Icon</Heading>
+        <Divider color="gradient" />
+        <For each={variants}>
+          {(variant) => (
+            <>
+              <Select
+                label="Fruit"
+                class="m-1"
+                icon="mdi:fruit-cherries"
+                size="small"
+                variant={variant}
+                options={options}
+                onChange={handleChange}
+              />
+              <Select
+                label="Fruit"
+                class="m-1"
+                icon="mdi:fruit-cherries"
+                size="small"
+                variant={variant}
+                options={options}
+                onChange={handleChange}
+              />
+            </>
+          )}
+        </For>
+      </Container>
+    </Container>
   );
 };
