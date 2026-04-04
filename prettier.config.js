2d55247d896a3cd266ca0cd12e36c05072a6699b
spuxx <accounts@spuxx.dev>
2026-04-04 17:46:04 +0200

    chore: Update some configs

---
+++ prettier.config.js
@@ -0,0 +1,11 @@
+/**
+ * @see https://prettier.io/docs/configuration
+ * @type {import("prettier").Config}
+ */
+const config = {
+  singleQuote: true,
+  trailingComma: 'all',
+  printWidth: 100,
+};
+
+export default config;
