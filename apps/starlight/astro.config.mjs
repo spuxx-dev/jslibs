f75a48ea8241a9d7926d4e2327f14c2eafa9f216
@LeopoldHock <leopold.hock@deutschebahn.com>
2026-08-03 16:40:42 +0200

    chore(starlight): Update starlight app

--- apps/starlight/astro.config.mjs
+++ apps/starlight/astro.config.mjs
@@ -2,7 +2,6 @@
 import { defineConfig } from 'astro/config';
 import solidJs from '@astrojs/solid-js';
 import starlight from '@astrojs/starlight';
-import { sidebar } from './src/assets/sidebar.ts';
 
 // https://astro.build/config
 export default defineConfig({
@@ -27,7 +26,77 @@ export default defineConfig({
         },
       ],
       customCss: ['./src/styles/global.css'],
-      sidebar,
+      sidebar: [
+        {
+          label: 'js-utils',
+          items: [
+            { label: 'Introduction', slug: 'js-utils' },
+            {
+              label: 'Types',
+              slug: 'js-utils/types',
+            },
+            {
+              label: 'Services',
+              items: [{ autogenerate: { directory: 'js-utils/services' } }],
+            },
+            {
+              label: 'Utilities',
+              items: [{ autogenerate: { directory: 'js-utils/utils' } }],
+            },
+          ],
+        },
+        {
+          label: 'browser-utils',
+          items: [
+            {
+              label: 'Introduction',
+              slug: 'browser-utils',
+            },
+            {
+              label: 'Styles and Themes',
+              slug: 'browser-utils/styles-and-themes',
+            },
+            {
+              label: 'Types',
+              items: [{ autogenerate: { directory: 'browser-utils/types' } }],
+            },
+            {
+              label: 'Services',
+              items: [{ autogenerate: { directory: 'browser-utils/services' } }],
+            },
+          ],
+        },
+        {
+          label: 'solid',
+          items: [
+            {
+              label: 'Introduction',
+              slug: 'solid',
+            },
+            {
+              label: 'Components',
+              items: [
+                {
+                  label: 'Control',
+                  items: [{ autogenerate: { directory: 'solid/components/control' } }],
+                },
+                {
+                  label: 'Layout',
+                  items: [{ autogenerate: { directory: 'solid/components/layout' } }],
+                },
+                {
+                  label: 'Typography',
+                  items: [{ autogenerate: { directory: 'solid/components/typography' } }],
+                },
+              ],
+            },
+            {
+              label: 'Layout',
+              items: [{ autogenerate: { directory: 'solid/layout' } }],
+            },
+          ],
+        },
+      ],
     }),
   ],
 });
